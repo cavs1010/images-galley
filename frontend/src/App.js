aa7fec848c0f9730b2b564380bfef0bdde5b6811
Camilo <cavs1010@gmail.com>
2022-05-01 13:11:20 +1000

    welcome component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Search from './components/Search';
 import ImageCard from './components/imagesCard';
 import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Welcome from './components/Welcome';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -35,17 +36,21 @@ const App = () => {
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
       <Container className="mt-4" />
       <Container>
-        <Row xs={1} md={2} lg={3}>
-          {images.map((image, i) => (
-            <Col key={i} className="pb-3">
-              <ImageCard
-                key={i}
-                image={image}
-                deleteImage={handleDeleteImage}
-              />
-            </Col>
-          ))}
-        </Row>
+        {images.length ? (
+          <Row xs={1} md={2} lg={3}>
+            {images.map((image, i) => (
+              <Col key={i} className="pb-3">
+                <ImageCard
+                  key={i}
+                  image={image}
+                  deleteImage={handleDeleteImage}
+                />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Welcome />
+        )}
       </Container>
     </div>
   );
